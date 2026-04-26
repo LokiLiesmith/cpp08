554a2f3a478485bd8267f2075593745c4cc2e4a1
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2026-04-26 16:56:17 +0200

    ex00 - tests, lookup function, try catch blocks

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,34 +6,34 @@
 #include <algorithm>
 
 template<typename T>
-void printVec(std::vector<T>& vec)
+void printVec(const std::vector<T>& vec)
 {
+	std::cout << "{";
 	for (typename std::vector<T>::const_iterator it = vec.begin(); it != vec.end(); it++)
 	{
-		// std::cout << vec[it] << std::endl;
-		std::cout << *it << std::endl;
+		std::cout << *it;
+		if (std::next(it) != vec.end())
+			std::cout << ", ";
 	}
+	std::cout << "}" << std::endl;
 	// for (auto it = vec.begin(); it!=vec.end(); it++)
 	// 	std::cout << *it << "auto" << std::endl;
 
 }
 
+template<typename T>
+void printContainer(const T& cont)
+{
+	std::cout << "{";
+	for (typename T::const_iterator it = cont.begin(); it != cont.end(); it++)
+	{
+		std::cout << *it;
+		if (std::next(it) != cont.end())
+			std::cout << ", ";
+	}
+	std::cout << "}" << std::endl;
 
-// template <typename T>
-// typename std::vector<T>::iterator easyfind(std::vector<T>& container, int needle)
-// {
-// 	// for(int i = 0; (unsigned long)i < container->size(); i++)
-// 	// {
-// 	// 	std::cout << "easyfind[" << i << "]: "<< *(container->begin() + (unsigned long)i) << std::endl;
-// 	// }
-// 	for (auto it = container.begin(); it != container.end(); it++)
-// 	{
-// 		if(*it == needle)
-// 			return it;
-// 		// std::cout << needle << std::endl;
-// 	}
-// 	throw std::runtime_error("Value not found");
-// }
+}
 
 template<typename T>
 typename T::iterator easyfind(T& container, int needle)
@@ -45,17 +45,17 @@ typename T::iterator easyfind(T& container, int needle)
 	return it;
 }
 
-// template<typename T>
-// typename T::iterator easyfind(T& container, int needle)
-// {
-// 	typename T::iterator it;
-// 	it = std::find(container.begin(), container.end(), needle);
+//Const overload
+template<typename T>
+typename T::const_iterator easyfind(const T& container, const int needle)
+{
+	typename T::const_iterator it = std::find(container.begin(), container.end(), needle);
 
-// 	if (it == container.end())
-// 		throw std::runtime_error("Value not found");
-
-// 	return it;
-// }
+	if (it == container.end())
+		throw std::runtime_error("Value not found");
+	
+	return it;
+}
 
 template<typename T>
 typename T::iterator easierfind(T& container, typename T::value_type needle)
@@ -66,6 +66,26 @@ typename T::iterator easierfind(T& container, typename T::value_type needle)
 		throw std::runtime_error("Value not found");
 	
 	return it;
+}
+
+//LOOKUP FOR ALL CONTAINERS
+template<typename T>
+void lookup(T& cont, int i, std::string name)
+{
+	try
+	{
+		std::cout << "Looking for: " << i << " in " << name << std::endl;
+		typename T::const_iterator it = easyfind(cont, i);
+		int index = 0;
+		for (typename T::const_iterator tmp = cont.begin(); tmp != it; ++tmp)
+			++index;
+		std::cout << "found at: " << name << "[" << index << "]" << std::endl;
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << name << ": " << e.what() << std::endl;
+	}
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,54 +1,87 @@
 #include "easyfind.hpp"
 #include <vector>
+#include <list>
 #include <algorithm>
 
 int main(void)
 {
 
-	std::vector<int> container;
-	
-	std::cout << "container size:		" << container.size() << std::endl;
-		std::cout << "Filling container!" << std::endl;
-	for (int i = 10; i < 100; i += 10)
-		container.push_back(i);
-	
-	std::cout << "container size:		" << container.size() << std::endl;
-	std::cout << "vector address:		" << &container << std::endl;
-	std::cout << "first object address:	" << &(*container.begin()) << std::endl;
-	std::cout << "container[0]:		" << *container.begin() << std::endl;
-	std::cout << "container[1]:		" << *(container.begin() + 1) << std::endl;
+	std::vector<int> vec;
+	// std::vector<int>::iterator it;
+	// std::cout << "vector size:		" << vector.size() << std::endl;
+	std::cout << "Filling vector!" << std::endl;
+	for (int i = 10; i <= 100; i += 10)
+		vec.push_back(i);
+	printVec(vec);
 
-	printVec(container);
+	// std::cout << "vec size:		" << vec.size() << std::endl;
+	// std::cout << "vec address:		" << &vec << std::endl;
+	// std::cout << "first object address:	" << &(*vec.begin()) << std::endl;
+	// std::cout << "vec[0]:		" << *vec.begin() << std::endl;
+	// std::cout << "vec[1]:		" << *(vec.begin() + 1) << std::endl;
+	// 1. find first element
+	std::cout << "\n*** FIRST ELEMENT ***" << std::endl;
+	lookup(vec, 10, "vec");
 
 
-	std::vector<int> arr_1;
-	arr_1.push_back(10);
-	if (arr_1.size() > 0)
-	{
-		std::vector<int>::iterator start = arr_1.begin();
-		std::cout << *start << std::endl;
-	}
+	// 2. find last element
+	std::cout << "\n*** LAST ELEMENT ***" << std::endl;
+	lookup(vec, 100, "vec");
 
-	// printVec(container);
-	// printVec(arr_1);
+	//3. duplicate values
+	std::cout << "\n*** DUPLICATE VALUE: 60 ***" << std::endl;
+	vec.push_back(60);
+	printVec(vec);
+	lookup(vec, 60, "vec");
+	// it = easyfind(vec, 60);
+	// std::cout << "first 60 index: " << it - vec.begin() << std::endl;
 
-	// easyfind(container, 2);
-
-	std::cout << "\n*** EASYFIND ***" << std::endl;
+	//4. Empty container: should throw
+	std::cout << "\n*** EMPTY CONTAINER ***" << std::endl;
+	std::vector<int> empty;
 	try
 	{
-		std::cout << *easyfind(container, 60) << std::endl;
-		std::cout << *easyfind(container, 66) << std::endl;	
+		// easyfind(empty, 10);
+		lookup(empty, 10, "empty");
 	}
 	catch(const std::exception& e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "empty test: " << e.what() << std::endl;
 	}
+
+	// 5. List test
+	std::cout << "\n*** LIST TEST ***" << std::endl;
+	std::list<int> lst;
+	lst.push_back(1);
+	lst.push_back(2);
+	lst.push_back(3);
+	printContainer(lst);
+	// std::cout << *easyfind(list, 2) << std::endl;
+	lookup(lst, 2, "lst");
+
+	// 6. CONST CONTAINER
+	std::cout << "\n*** CONST CONTAINER ***" << std::endl;
+		const std::vector<int> const_container(vec);
+	printContainer(const_container);
+	lookup(const_container, 60, "const_container");
+	lookup(const_container, 66, "const_container");
 	
-	std::cout << "\n*** EASIERFIND ***" << std::endl;
+	
+
+	/// 7. STRING BLOCK - EASIER FIND
+	std::vector<std::string> container_of_strings;
+	std::cout << "\n*** EASIERFIND - STRING ***" << std::endl;
+	container_of_strings.push_back("Mark");
+	container_of_strings.push_back("what");
+	container_of_strings.push_back("the");
+	container_of_strings.push_back("hell");
+	container_of_strings.push_back("is");
+	container_of_strings.push_back("this?");
+	
 	try
 	{
-		std::cout << *easierfind(container, 60) << std::endl;
+		std::cout << *easierfind(container_of_strings, "Mark") << std::endl;
+		std::cout << *easierfind(container_of_strings, "60") << std::endl;
 	}
 	catch(const std::exception& e)
 	{
